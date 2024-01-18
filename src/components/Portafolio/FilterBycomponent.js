28cf23bd65517dfbd9add63f88793fed0468ca3c
duvan51 <duvanaponteramirez@gmail.com>
2024-01-17 19:03:01 -0500

    subida duvan

--- src/components/Portafolio/FilterBycomponent.js
+++ src/components/Portafolio/FilterBycomponent.js
@@ -11,12 +11,17 @@ const FilterBycomponent = ({ data }) => {
   
   return (
     <>
-    {data ? (
-        <>
-        {  data.map((x) => {
-          const style = {
-            backgroundImage: `url(${x.fondo})`,
-          };
+  {data ? (
+    <>
+      {data.map((x) => {
+        const style = {
+          backgroundImage: `url(${x.fondo})`,
+        };
+
+        const handleClick = () => {
+          window.open(x.link, '_blank', 'noreferrer');
+        };
+
         return (
           <div className="proyectFrontOne" style={style} key={x.id}>
             <div className="cardName">
@@ -26,26 +31,25 @@ const FilterBycomponent = ({ data }) => {
               <h2 className="CardInfoTitle">{x.name}</h2>
               <div className="cardInfoText">{x.description}</div>
               <div className="CardInfoImages">
-                <div>Tecnologias ussed</div>
+                <div>Tecnologias usadas</div>
                 <div className="CardInfoImag">
                   {x.images.map((img) => {
                     return <img key={img} src={img} alt="" />;
                   })}
                 </div>
               </div>
-              <a className="cardIcon" href={x.link} target="_blank">
+              <div className="cardIcon" onClick={handleClick}>
                 <div className="iconLink">
-                    <i class="fi fi-sr-play"></i>
+                  <i className="fi fi-sr-play"></i>
                 </div>
-              </a>
+              </div>
             </div>
           </div>
         );
       })}
-        
-        </>
-    ): null}  
     </>
+  ) : null}
+</>
   );
 };
 
